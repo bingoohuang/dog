17547af2e0307542e8b855f47ed40110eba00f10
bingoohuang <bingoo.huang@gmail.com>
2021-07-14 16:53:13 +0800

    watch dog

--- cmd/dog/main.go
+++ cmd/dog/main.go
@@ -1,55 +1,79 @@
 package main
 
 import (
+	"embed"
 	"fmt"
-	"os"
-	"strings"
-
-	"github.com/bingoohuang/gou/htt"
-	"github.com/bingoohuang/gou/lo"
-	"github.com/mitchellh/go-homedir"
-	"github.com/sirupsen/logrus"
-	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
+	"github.com/bingoohuang/dog"
+	"github.com/bingoohuang/gg/pkg/ctl"
+	"github.com/bingoohuang/gg/pkg/flagparse"
+	"github.com/bingoohuang/golog"
+	"runtime"
+	"time"
 )
 
-func main() {
-	parseFlags()
+func (Config) VersionInfo() string { return "dog v1.0.0 2021-07-14 16:50:32" }
 
-	CreateAgApp().GoStart()
-	select {}
+func (c Config) Usage() string {
+	return fmt.Sprintf(`Usage of dog:
+  -filter value 命令包含，以!开头为不包含，可以多个值
+  -kill string 发送信号，eg INT TERM KILL QUIT USR1 USR2 (default "INT")
+  -max-mem value 允许最大内存 (默认 0B，不检查内存)
+  -max-pcpu int 允许内存最大百分比, eg 1-%d (默认 %d), 0 不检查 CPU
+  -max-pmem int 允许CPU最大百分比, eg 1-100 (默认 50)
+  -pid int 指定pid
+  -ppid int 指定ppid
+  -self 是否监控自身
+  -span duration 检查时间间隔 (默认 10s)
+  -topn int 只取前N个检查
+  -v Print version info and exit\n`, runtime.NumCPU()*100, runtime.NumCPU()*50)
 }
 
-func parseFlags() {
-	conf := pflag.StringP("conf", "c", "./conf.toml", "config file path")
-	help := pflag.BoolP("help", "h", false, "help")
-	pflag.StringP("addr", "", ":9910", "listen address, eg :9910")
-	pprofAddr := htt.PprofAddrPflag()
-	pflag.Parse()
-	args := pflag.Args()
-	if len(args) > 0 {
-		fmt.Printf("Unknown args %s\n", strings.Join(args, " "))
-		pflag.PrintDefaults()
-		os.Exit(0)
+type Config struct {
+	Config string `flag:"c" usage:"yaml config filepath"`
+	Init   bool   `usage:"init example dog.yml/ctl and then exit"`
+
+	Topn    int
+	Pid     int
+	Ppid    int
+	Self    bool
+	Kill    string        `val:"INT"`
+	Span    time.Duration `val:"10s"`
+	MaxMem  uint64        `size:"true"`
+	MaxPmem int           `val:"50"`
+	MaxPcpu int
+	Filter  []string
+
+	Version bool `flag:"v" usage:"Print version info and exit"`
+}
+
+func (c *Config) PostProcess() {
+	if c.MaxPcpu == 0 {
+		c.MaxPcpu = runtime.NumCPU() * 50
 	}
-	if *help {
-		pflag.PrintDefaults()
-		os.Exit(0)
+}
+
+//go:embed initassets
+var initAssets embed.FS
+
+func main() {
+	c := &Config{}
+	flagparse.Parse(c, flagparse.AutoLoadYaml("c", "dog.yml"))
+	ctl.Config{Initing: c.Init, InitFiles: initAssets}.ProcessInit()
+	golog.SetupLogrus()
+
+	watchConfig := dog.WatchConfig{
+		Topn:       c.Topn,
+		Pid:        c.Pid,
+		Ppid:       c.Ppid,
+		Self:       c.Self,
+		BitSignals: c.Kill,
+		Interval:   c.Span,
+		MaxMem:     c.MaxMem,
+		MaxPmem:    float32(c.MaxPmem),
+		MaxPcpu:    float32(c.MaxPcpu),
+		CmdFilter:  c.Filter,
 	}
 
-	if *conf != "" {
-		cf, _ := homedir.Expand(*conf)
-		if _, err := os.Stat(cf); err == nil {
-			viper.SetConfigFile(*conf)
-		}
-	}
-
-	htt.StartPprof(*pprofAddr)
-	viper.SetEnvPrefix("DOG")
-	viper.AutomaticEnv()
-
-	// 绑定命令行参数，（优先级比配置文件高）
-	lo.Err(viper.BindPFlags(pflag.CommandLine))
-
-	logrus.SetLevel(logrus.InfoLevel)
+	d := dog.NewDog(dog.WithConfig(watchConfig))
+	d.StartWatch()
 }
--- psaux.go
+++ psaux.go
@@ -2,45 +2,109 @@ package dog
 
 import (
 	"fmt"
-	"os"
-	"strings"
-	"time"
-
+	"github.com/bingoohuang/gg/pkg/man"
 	"github.com/bingoohuang/gou/str"
+	"log"
+	"regexp"
+	"strconv"
+
+	"github.com/bingoohuang/gg/pkg/ss"
 	"github.com/gobars/cmd"
-	"github.com/sirupsen/logrus"
 )
 
-type PsauxOut struct {
-	Pid    int
-	Pcpu   uint32
-	Pmem   uint32
-	VszKib uint32
-	RssKib uint32
-	Line   string
+// PsAuxItem ...
+type PsAuxItem struct {
+	User    string
+	Pid     int
+	Ppid    int
+	Pcpu    float32
+	Pmem    float32
+	Vsz     uint64
+	Rss     uint64
+	Tty     string
+	Stat    string
+	Start   string
+	Time    string
+	Command string
 }
 
-func PsauxSelf() PsauxOut {
-	return Psaux(uint32(os.Getpid()))
+func (p PsAuxItem) String() string {
+	return fmt.Sprintf("User: %s Pid: %d Ppid: %d %%cpu: %s %%mem: %s VSZ: %s, RSS: %s Tty: %s Stat: %s Start: %s Time: %s Command: %s",
+		p.User, p.Pid, p.Ppid,
+		strconv.FormatFloat(float64(p.Pcpu), 'f', -1, 32),
+		strconv.FormatFloat(float64(p.Pmem), 'f', -1, 32),
+		man.Bytes(p.Vsz), man.Bytes(p.Rss), p.Tty, p.Stat, p.Start, p.Time, p.Command)
 }
 
-func Psaux(pid uint32) PsauxOut {
-	ps := fmt.Sprintf(`ps aux |awk '$2 == %d { print $0 }'`, pid)
+// PsAuxTop ...
+func PsAuxTop(n, printN int) ([]PsAuxItem, error) {
+	auxItems := make([]PsAuxItem, 0)
+	re := regexp.MustCompile(`\s+`)
+	i := 0
+	_, status := cmd.BashLiner(PasAuxShell(n, false), func(line string) bool {
+		f := re.Split(line, 13)
+		item := PsAuxItem{
+			User:    f[2],
+			Pid:     ss.ParseInt(f[3]),
+			Ppid:    ss.ParseInt(f[4]),
+			Pcpu:    ss.ParseFloat32(f[5]),
+			Pmem:    ss.ParseFloat32(f[6]),
+			Vsz:     ss.ParseUint64(f[7]) * 1024,
+			Rss:     ss.ParseUint64(f[8]) * 1024,
+			Tty:     f[9],
+			Stat:    f[10],
+			Start:   f[0] + ` ` + f[1],
+			Time:    f[11],
+			Command: f[12],
+		}
 
-	_, stat := cmd.Bash(ps, cmd.Timeout(1*time.Second))
-	if stat.Error != nil {
-		logrus.Errorf("exec %s error %v", ps, stat)
-		return PsauxOut{}
-	}
+		if i < printN {
+			log.Printf("%s", item)
+		}
+		i++
 
-	output := stat.Stdout[0]
-	n := strings.Fields(output)
-	return PsauxOut{
-		Pid:    str.ParseInt(n[1]),
-		Pcpu:   str.ParseUint32(n[2]),
-		Pmem:   str.ParseUint32(n[3]),
-		VszKib: str.ParseUint32(n[4]),
-		RssKib: str.ParseUint32(n[5]),
-		Line:   output,
-	}
+		auxItems = append(auxItems, item)
+		return true
+	})
+
+	return auxItems, status.Error
 }
+
+// PasAuxShell ...
+func PasAuxShell(topN int, heading bool) string {
+	return prefix + str.If(heading, "", noheading) + psAuxTopOpt(topN) + fixedLtime
+}
+
+/*
+ps是linux系统的进程管理工具，般来说，ps aux命令执行结果的几个列的信息分别是：
+
+USER 进程所属用户
+PID 进程ID
+%CPU 进程占用CPU百分比
+%MEM 进程占用内存百分比
+VSZ 虚拟内存占用大小
+RSS 实际内存占用大小
+TTY 终端类型
+STAT 进程状态
+START 进程启动时刻
+TIME 进程运行时长
+COMMAND 启动进程的命令
+
+https://superuser.com/a/117921
+
+USER = user owning the process
+PID = process ID of the process
+%CPU = It is the CPU time used divided by the time the process has been running.
+%MEM = ratio of the process’s resident set size to the physical memory on the machine
+VSZ = virtual memory usage of entire process (in KiB)
+RSS = resident set size, the non-swapped physical memory that a task has used (in KiB)
+TTY = controlling tty (terminal)
+STAT = multi-character process state
+START = starting time or date of the process
+TIME = cumulative CPU time
+COMMAND = command with all its arguments
+
+https://alvinalexander.com/linux/unix-linux-process-memory-sort-ps-command-cpu/
+vsz        VSZ      virtual memory size of the process in KiB (1024-byte units). Device mappings are currently excluded; this is subject to change.
+rss        RSS      resident set size, the non-swapped physical memory that a task has used (in kiloBytes).
+*/
